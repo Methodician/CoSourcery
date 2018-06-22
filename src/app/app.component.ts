a54b11f6750a858787b740275555470b6861a1d6
Methodician <jacobgjohnston@gmail.com>
2018-06-22 16:51:57 -0700

    Firebase installed and tested in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as fb from 'firebase';
 
 @Component({
   selector: 'cos-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cos';
+
+  data = {};
+  config = {
+    apiKey: "AIzaSyBn8hJ2vDLN21aUl9cP-RgeOWZHZOlbtdY",
+    authDomain: "scatterschool-dev.firebaseapp.com",
+    databaseURL: "https://scatterschool-dev.firebaseio.com",
+    projectId: "scatterschool-dev",
+    storageBucket: "scatterschool-dev.appspot.com",
+    messagingSenderId: "945815872407"
+  };
+
+  constructor() {
+    fb.initializeApp(this.config);
+    const db = fb.database();
+    db.ref('userInfo/open/9u5CzLBL6efjwhM9CPsetQyb62W2').on('value', (snap) => {
+      this.data = snap.val();
+    })
+  }
 }
