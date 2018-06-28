ee5c6419f5f18181e7a48e0c31668ea8a84518c4
Ian Goodrich <iangoodrich@hotmail.com>
2018-06-28 15:03:09 -0700

    update suggestions and suggestions-vote and preview compoenents. scss issues with vote.

--- src/app/components/suggestions/suggestion-preview/suggestion-preview.component.ts
+++ src/app/components/suggestions/suggestion-preview/suggestion-preview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SuggestionService } from '../../../services/suggestion.service';
 
 @Component({
   selector: 'cos-suggestion-preview',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuggestionPreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: SuggestionService) { }
+  @Input() suggestion;
+  @Input() currentUserKey;
 
   ngOnInit() {
   }
--- src/app/components/suggestions/suggestions/suggestions.component.ts
+++ src/app/components/suggestions/suggestions/suggestions.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SuggestionService } from '../../../services/suggestion.service';
+import { Suggestion } from '../../../shared/class/suggestion';
+import { SortOptions } from 'app/shared/pipes/suggestion-sort.pipe';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'cos-suggestions',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suggestions.component.scss']
 })
 export class SuggestionsComponent implements OnInit {
-
-  constructor() { }
+suggestions: Suggestion[];
+currentSortOption: SortOptions = SortOptions.upvotes;
+currentUserKey: string;
+  constructor(private suggestionSvc: SuggestionService, private authSvc: AuthService) { }
 
   ngOnInit() {
+    window.scrollTo(0, 0);
+
+    this.authSvc.authInfo.subscribe(authInfo => {
+      this.currentUserKey = (authInfo.uid) ? authInfo.uid : null;
+    });
+
+    this.suggestionSvc.getAllSuggestions().then(data =>
+      this.suggestions = data
+    );
   }
+
+  isSelected(sortOption: string) {
+    return SortOptions[sortOption] === this.currentSortOption;
+  }
+
+  sortBy(sortOption: string) {
+    this.currentSortOption = SortOptions[sortOption];
+  }
+
 
 }
