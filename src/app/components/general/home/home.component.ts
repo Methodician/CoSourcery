af6174457d3618425c964b0daf7aea3aee9ec78f
Ian Goodrich <iangoodrich@hotmail.com>
2018-06-28 13:32:06 -0700

    update suggestion service

--- src/app/components/general/home/home.component.ts
+++ src/app/components/general/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../../../services/article.service';
-
+import { SuggestionService } from '../../../services/suggestion.service';
 @Component({
   selector: 'cos-home',
   templateUrl: './home.component.html',
@@ -9,16 +9,24 @@ import { ArticleService } from '../../../services/article.service';
 export class HomeComponent implements OnInit {
 
   testData: any;
+  testDataB: any;
   constructor(
-    private articleSvc: ArticleService
+    private articleSvc: ArticleService,
+    private SuggSvc: SuggestionService,
   ) { }
 
   ngOnInit() {
-    // this.testData = this.articleSvc.getAllArticles();
-    // this.testData = this.articleSvc.getLatestArticles();
-    // this.testData = this.articleSvc.getArticleById('-KvdKvEN1RqI-Nykd55g');
-    // this.testData = this.articleSvc.getArticleBody('-KvdKvEJp15zRas8xKWr');
-    this.testData = this.articleSvc.getFullArticleById('-KvdKvEN1RqI-Nykd55g');
+    // const suggSlugg = {title: 'test suggestion', pitch: 'hire me for lots of money to write code and content'};
+    // this.testData = this.SuggSvc.saveSuggestion(suggSlugg);
+    // const suggSluggB = {title: 'WOOOT!', pitch: 'Hire me and Nico for lots of money to write code and content'};
+    // this.SuggSvc.updateSuggestion('SfSVPu2oE8NOp0YD9SD0', suggSluggB);
+    // this.testData = this.SuggSvc.getAllSuggestions();
+    // console.log(this.testData);
+    // this.testDataB = this.SuggSvc.getSuggestionById('SfSVPu2oE8NOp0YD9SD0'),
+    // console.log(this.testDataB);
+
   }
+
+
 
 }
--- src/app/services/suggestion.service.ts
+++ src/app/services/suggestion.service.ts
@@ -1,9 +1,75 @@
 import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import * as firestore from 'firebase';
+//  Docs say to do this - "required for side-effects" whatever that means
+// require('firebase/firestore');
+import 'firebase/firestore';
+import { Route, Router } from '@angular/router';
+import { Firestore } from '@google-cloud/firestore';
+import { forEach } from '@angular/router/src/utils/collection';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuggestionService {
 
-  constructor() { }
+  fsdb: any;
+
+  constructor(private router: Router) {
+    this.fsdb = firebase.firestore();
+  }
+
+
+  async getAllSuggestions() {
+    const suggestionsList = [];
+    const querySnap = await this.fsdb.collection('suggestions').get()
+    .then(docs => {
+      docs.forEach(doc => {
+        suggestionsList.push(doc.data());
+      });
+    });
+    return suggestionsList;
+  }
+
+  async getSuggestionById(suggId: string) {
+    const suggRef = this.fsdb.doc(`suggestions/${suggId}`);
+    const docSnapshot = await suggRef.get();
+    return docSnapshot.data();
+  }
+
+  saveSuggestion(suggestion) {
+    const docRef = this.fsdb.collection(`suggestions`).doc();
+    const id = docRef.id;
+    suggestion.id = id;
+    suggestion.timestamp = firebase.firestore.FieldValue.serverTimestamp();
+    suggestion.lastUpdated = firebase.firestore.FieldValue.serverTimestamp();
+    suggestion.voteCount = 0;
+
+    this.fsdb.collection(`suggestions`).doc(id).set(suggestion);
+    this.router.navigate([`suggestions`]);
+
+  }
+
+  updateSuggestion(id: string, updatedSuggestion) {
+    updatedSuggestion.lastUpdated = firebase.firestore.FieldValue.serverTimestamp();
+    this.fsdb.collection(`suggestions`).doc(id)
+    .update(updatedSuggestion);
+    this.router.navigate([`suggestions`]);
+  }
+
+
+
+
+  arrayFromCollectionSnapshot(querySnapshot: any, shouldAttachId: boolean = false) {
+    const array = [];
+    querySnapshot.forEach(doc => {
+      if (shouldAttachId) {
+        array.push({ id: doc.id, ...doc.data() });
+      } else {
+        array.push(doc.data());
+      }
+    });
+    return array;
+  }
+
 }
