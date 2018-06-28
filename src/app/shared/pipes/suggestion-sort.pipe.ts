162dc9f25395069abb2d2a1aa6c23fd4565a5500
Ian Goodrich <iangoodrich@hotmail.com>
2018-06-28 14:58:24 -0700

    updated suggestion-sort-pipe

--- src/app/shared/pipes/suggestion-sort.pipe.ts
+++ src/app/shared/pipes/suggestion-sort.pipe.ts
@@ -5,8 +5,31 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SuggestionSortPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(suggestions: any[], sortType?: SortOptions): any {
+    if (!suggestions) {
+      return null;
+    }
+    switch (sortType) {
+      case SortOptions.upvotes:
+        return suggestions.sort((a, b) => {
+          return b.voteCount - a.voteCount;
+        });
+      case SortOptions.newest:
+        return suggestions.sort((a, b) => {
+          return b.lastUpdated - a.lastUpdated;
+        });
+      case SortOptions.oldest:
+        return suggestions.sort((a, b) => {
+          return a.lastUpdated - b.lastUpdated;
+        });
+      default:
+        return suggestions;
+    }
   }
+}
 
+export enum SortOptions {
+  'upvotes' = 1,
+  'newest',
+  'oldest'
 }
