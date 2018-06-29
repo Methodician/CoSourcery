570aaf54df18c26ff34045af69a354ec5d7c2a9c
Ian Goodrich <iangoodrich@hotmail.com>
2018-06-29 13:27:41 -0700

    re-implemented/ refactored suddestion-edit

--- src/app/components/suggestions/suggestion-edit/suggestion-edit.component.ts
+++ src/app/components/suggestions/suggestion-edit/suggestion-edit.component.ts
@@ -1,4 +1,7 @@
+import { Suggestion } from '../../../shared/class/suggestion';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SuggestionService } from '../../../services/suggestion.service';
 
 @Component({
   selector: 'cos-suggestion-edit',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./suggestion-edit.component.scss']
 })
 export class SuggestionEditComponent implements OnInit {
-
-  constructor() { }
+  suggestion: Suggestion;
+  constructor(private route: ActivatedRoute, private suggestionSvc: SuggestionService, ) { }
 
   ngOnInit() {
+    const key = this.route.snapshot.params['key'];
+    this.suggestionSvc
+      .getSuggestionById(key)
+      .then(suggestion => {
+        this.suggestion = suggestion;
+      });
   }
 
+
+  saveSuggestion(suggestionData) {
+    this.suggestionSvc.updateSuggestion(this.suggestion.id, suggestionData);
+  }
 }
