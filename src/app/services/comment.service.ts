4bb2020467d1563662d6c3135b0ba44ce8976f92
Ian Goodrich <iangoodrich@hotmail.com>
2018-06-29 16:00:02 -0700

    working on comments service

--- src/app/services/comment.service.ts
+++ src/app/services/comment.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 //  Docs say to do this - "required for side-effects" whatever that means
 // require('firebase/firestore');
@@ -10,7 +11,24 @@ import 'firebase/firestore';
 export class CommentService {
   fsdb: any;
 
-  constructor() {
-    this.fsdb = firebase.firestore();
+  constructor(private router: Router) {
+    this.fsdb = firebase.database();
   }
+  saveComment(commentData) {
+    const commentToSave = {
+      authorKey: commentData.authorKey,
+      parentKey: commentData.parentKey,
+      parentType: commentData.parentType,
+      text: commentData.text,
+      timestamp: firebase.database.ServerValue.TIMESTAMP,
+      lastUpdated: firebase.database.ServerValue.TIMESTAMP,
+      commentCount: 0
+    };
+
+    firebase.database().ref('commentData/comments').set(commentToSave);
+    // this.updateCommentCount(commentData.parentKey, commentData.parentType, 1);
+  }
+
+
+
 }
