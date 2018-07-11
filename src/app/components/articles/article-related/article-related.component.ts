0ce2ac497d206e2f7d4fada765a9a35a43dc277f
Ian Goodrich <iangoodrich@hotmail.com>
2018-07-11 13:51:12 -0700

    didn't mean to add this one. But it's there to be worked on next

--- src/app/components/articles/article-related/article-related.component.ts
+++ src/app/components/articles/article-related/article-related.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { ArticleService } from '../../../services/article.service';
 
 @Component({
   selector: 'cos-article-related',
   templateUrl: './article-related.component.html',
   styleUrls: ['./article-related.component.scss']
 })
-export class ArticleRelatedComponent implements OnInit {
+export class ArticleRelatedComponent implements OnInit, OnChanges {
+  @Input()  any;
+  @Input() parentTags: any;
+  relatedArticles: any;
+  currentArticle = 0;
 
-  constructor() { }
+  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
+  constructor(private articleSvc: ArticleService) {}
 
   ngOnInit() {
   }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['parentTags'] && changes['parentTags'].currentValue) {
+      this.relatedArticles = this.articleSvc.getArticlesPerTag(this.parentTags);
+      this.currentArticle = 0;
+    }
+  }
+
+  // nextArticle(){
+  //   if(this.currentArticle != this..length - 1){
+  //     this.currentArticle++;
+  //   }
+  // }
+
+  // prevArticle(){
+  //   if(this.currentArticle != 0){
+  //     this.currentArticle--;
+  //   }
+  // }
+  // positionWrapper():string{
+  //   return `${-320 * this.currentArticle}px`;
+  // }
+
+  // swipe(action = this.SWIPE_ACTION.RIGHT) {
+  //   if (action === this.SWIPE_ACTION.RIGHT) {
+  //     this.prevArticle();
+  //   }
+  //   if (action === this.SWIPE_ACTION.LEFT) {
+  //     this.nextArticle();
+  //   }
+  // }
+
+  // hasRelatedArticles(){
+  //   if (this.){
+  //     return this..length === 0 ?  true : false;
+  //   }
+  // }
 
 }
