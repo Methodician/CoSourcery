233cc56a2e194b716aef3f385bb24abbf76bcb9d
Ian Goodrich <iangoodrich@hotmail.com>
2018-07-11 13:46:11 -0700

    add home compoenent for article display

--- src/app/components/general/home/home.component.ts
+++ src/app/components/general/home/home.component.ts
@@ -1,24 +1,106 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../../../services/article.service';
-
+import { ActivatedRoute } from '@angular/router';
+import { ArticleDetailFirestore } from '../../../shared/class/article-info';
 @Component({
   selector: 'cos-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [ArticleService]
 })
 export class HomeComponent implements OnInit {
+  routeParams;
+  featuredArticles: ArticleDetailFirestore[];
+  latestArticles: ArticleDetailFirestore[];
+  allArticles: ArticleDetailFirestore[];
+  currentSelectedFeaturePreview: SelectedPreview = SelectedPreview.latestList;
+  currentSelectedLatestPreview: SelectedPreview = SelectedPreview.latestTile;
+  currentSelectedTab: SelectedTab = SelectedTab.latest;
+  currentSelectedAllPreview: SelectedPreview = SelectedPreview.latestTile;
 
-  testData: any;
-  constructor(
-    private articleSvc: ArticleService
-  ) { }
+  constructor(private route: ActivatedRoute, private articleSvc: ArticleService, ) { }
 
   ngOnInit() {
-    // this.testData = this.articleSvc.getAllArticles();
-    // this.testData = this.articleSvc.getLatestArticles();
-    // this.testData = this.articleSvc.getArticleById('-KvdKvEN1RqI-Nykd55g');
-    // this.testData = this.articleSvc.getArticleBody('-KvdKvEJp15zRas8xKWr');
-    this.testData = this.articleSvc.getFullArticleById('-KvdKvEN1RqI-Nykd55g');
+    this.initializeArticles();
+
+    // this.articleSvc
+    //   .getFeaturedArticles().then(articles => {
+    //     this.featuredArticles = articles;
+    //     // console.log(this.featuredArticles);
+    //   });
+
+    // // call For Latest Articles
+    // this.articleSvc
+    //   .getLatestArticles().then(latest => {
+    //     this.latestArticles = latest;
+    //     // console.log(this.latestArticles);
+    //   });
+
+    // //  call for All Articles
+    // this.articleSvc
+    //   .getAllArticles().then(responce => {
+    //   this.allArticles = responce; console.log(this.allArticles);
+    //   });
+
   }
 
+  async initializeArticles() {
+    // Doesn't even really need a comment since the code describes the action.
+    this.featuredArticles = await this.articleSvc.getFeaturedArticles();
+    this.latestArticles = await this.articleSvc.getLatestArticles();
+    this.allArticles = await this.articleSvc.getAllArticles();
+    console.log(   this.allArticles);
+  }
+
+  // Methods for toggling between Latest and All Previews
+  selectLatest() {
+    this.currentSelectedTab = SelectedTab.latest;
+  }
+
+  selectAll() {
+    this.currentSelectedTab = SelectedTab.all;
+    console.log('all');
+  }
+
+  // Methods for toggling between Featured Article List and Grid Previews
+  featuredListView() {
+    this.currentSelectedFeaturePreview = SelectedPreview.featuredList;
+  }
+
+  featuredTileView() {
+    this.currentSelectedFeaturePreview = SelectedPreview.featuredTile;
+  }
+
+  // Methods for toggling between Latest Article List and Grid Preview
+  latestListView() {
+    this.currentSelectedLatestPreview = SelectedPreview.latestList;
+  }
+
+  latestTileView() {
+    this.currentSelectedLatestPreview = SelectedPreview.latestTile;
+  }
+
+  // Methods for Toggling between All Article List and Grid Preview
+  allListView() {
+    this.currentSelectedAllPreview = SelectedPreview.allList;
+  }
+
+  allTileView() {
+    this.currentSelectedAllPreview = SelectedPreview.allTile;
+  }
+
+}
+
+export enum SelectedTab {
+  'latest' = 1,
+  'all'
+}
+
+export enum SelectedPreview {
+  'featuredList' = 1,
+  'featuredTile',
+  'latestList',
+  'latestTile',
+  'allList',
+  'allTile'
 }
