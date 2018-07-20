7693b7110d082e08ebbafd045956d84906bf2c40
Ian Goodrich <iangoodrich@hotmail.com>
2018-07-20 13:53:57 -0700

    updated with necessary logic. Promise I hand wrote this.

--- src/app/components/articles/article-post/article-post.component.ts
+++ src/app/components/articles/article-post/article-post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticleService } from '../../../services/article.service';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'cos-article-post',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article-post.component.scss']
 })
 export class ArticlePostComponent implements OnInit {
+authInfo;
+userInfo;
+article;
 
-  constructor() { }
+  constructor(
+    private articleSvc: ArticleService,
+    private router: Router,
+    private authSvc: AuthService,
+    private userSvc: UserService,
+  )  {
+    this.authSvc.authInfo$.subscribe(authInfo => {
+      this.authInfo = authInfo;
+    });
+    this.userSvc.userInfo$.subscribe(userInfo => {
+      this.userInfo = userInfo;
+    });
+  }
 
   ngOnInit() {
+  }
+
+  async save(article) {
+   this.articleSvc.createArticle(this.userInfo.$key, this.userInfo, article);
   }
 
 }
