9c3c6d677ed6d29f5650f5adf27eb8cec603248a
Ian Goodrich <iangoodrich@hotmail.com>
2018-08-17 16:15:59 -0700

    update to reflect changes

--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -1,12 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { Upload } from '../shared/class/upload';
-import { UserService } from './user.service';
-import { AuthService } from './auth.service';
-
-import { Observable } from 'rxjs';
-import { LoginComponent } from '../components/account/login/login.component';
-import { async } from 'rxjs/internal/scheduler/async';
 
 @Injectable()
 export class UploadService {
@@ -29,50 +23,31 @@ export class UploadService {
   uploadImage(upload: Upload, uid, basePath) {
     const storageRef = this.storage.ref(`${basePath}/${uid}`).put(upload.file);
     storageRef.on(firebase.storage.TaskEvent.STATE_CHANGED,
-     (uploading) => {
+     () => {
         const snap = storageRef.snapshot;
-          upload.progress = ((snap.bytesTransferred / snap.totalBytes) * 100).toFixed(0).toString();
+          upload.progress = ((snap.bytesTransferred / snap.totalBytes) * 100).toFixed(0);
+        snap.ref.getDownloadURL().then(url => {
+          if (basePath === 'uploads/articleCoverImages/') {
+            this.fsdb.collection(`articleData/articles/articles/`).doc(`${uid}`).set({imageUrl: url});
+            return url;
+          } else {
+            this.rtdb.ref(`userInfo/open/${uid}`).set({imageUrl: url});
+            return url;
+          }
+        });
      },
     (error) => {
       console.log('error: ', error.message);
       alert(error);
     },
     () => {
-      storageRef.snapshot.ref.getDownloadURL().then(url => {
-        if (basePath === 'uploads/articleCoverImages/') {
-          this.fsdb.collection(`articleData/articles/articles/`).doc(`${uid}`).update({imageUrl: url});
-        } else {
-          this.rtdb.ref(`userInfo/open/${uid}`).update({imageUrl: url});
-        }
-      });
+      console.log('image upload success');
     }
   );
  }
 
 
-  // NEEDS REFACTOR
-  //  async uploadImage(upload: Upload, uid, basePath) {
-    // delete old file from storage
-    // this is not working likely.
-    // if (upload.url) {
-    //   this.deleteFileStorage(uid, basePath);
-    // }
-    // const filePath = `${basePath}/${uid}`;
-    // try {
-    // const successSnap2 = await this.storage.upload(filePath, upload.file);
-    //   const url2 = await successSnap2.ref.getDownloadURL();
-    //   upload.url = url2;
-    //   upload.size = successSnap2.metadata.size;
-    //   upload.type = successSnap2.metadata.contentType;
-    //   upload.name = successSnap2.metadata.name;
-    //   upload.timeStamp = firebase.database.ServerValue.TIMESTAMP;
-    //   upload.progress = null;
-    //   this.saveImageData(upload, uid, basePath);
-    // } catch (err) {
-    //   console.log('errors!', err);
-    //     alert('There was an error saving this image.');
-  // }
 
-// }
+
 }
 
