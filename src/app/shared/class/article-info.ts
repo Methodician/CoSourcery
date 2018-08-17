53ef0bf0c1537ef3330486fe2a25af1dec79ee04
Ian Goodrich <iangoodrich@hotmail.com>
2018-08-17 12:21:29 -0700

    changed ArticleDetailFirestore and ArticleDetailPreview models.

--- src/app/shared/class/article-info.ts
+++ src/app/shared/class/article-info.ts
@@ -22,35 +22,34 @@ export class ArticleDetailPreview {
         public introduction: string,
         public lastUpdated: firebase.firestore.Timestamp,
         public timeStamp: firebase.firestore.Timestamp,
+        public imageUrl: string,
+        public imageAlt: string,
         public version: number,
         public commentCount: number,
         public viewCount: number,
         public tags?: string[],
-        public imgUrl?: string,
-        public imgAlt?: string
     ) { }
 }
 
 
 export class ArticleDetailFirestore {
     constructor(
+        public articleId: string,
         public authorId: string,
-        public bodyId: string,
         public title: string,
         public introduction: string,
+        public body: string,
+        public imageUrl: string,
+        public imageAlt: string,
+        public authorImageUrl: string,
         public lastUpdated: firebase.firestore.Timestamp,
         public timestamp: firebase.firestore.Timestamp,
+        public lastEditorId: string,
         public version: number,
         public commentCount: number,
         public viewCount: number,
         public tags?: string[],
-        public body?: string,
-        public articleId?: string,
         public isFeatured?: boolean,
-        public lastEditorId?: string,
-        public imgUrl?: string,
-        public imgAlt?: string,
-        public authorImgUrl?: string
     ) { }
 }
 
