65f42d662059b3804a24c2596eee2196c0533364
Ian Goodrich <iangoodrich@hotmail.com>
2018-08-20 13:44:08 -0700

    cleanout depricated logic.

--- src/app/components/articles/article-preview-card/article-preview-card.component.ts
+++ src/app/components/articles/article-preview-card/article-preview-card.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { UploadService } from '../../../services/upload.service';
-import { Router, ActivatedRoute, Params } from '@angular/router';
 import { ArticleService } from '../../../services/article.service';
-import { UserService } from '../../../services/user.service';
 import { AuthService } from '../../../services/auth.service';
 import { ArticleDetailFirestore } from 'app/shared/class/article-info';
 @Component({
@@ -12,8 +9,6 @@ import { ArticleDetailFirestore } from 'app/shared/class/article-info';
 })
 export class ArticlePreviewCardComponent implements OnInit {
   @Input() articleData: ArticleDetailFirestore;
-  author;
-  profileImageUrl;
   articleCoverImageUrl;
   // We should just be using and @Input() uid: string; instead of using the authSvc for every preview card... User is not needed.
   user;
@@ -22,16 +17,10 @@ export class ArticlePreviewCardComponent implements OnInit {
   hoverBg: string;
   constructor(
     private articleSvc: ArticleService,
-    private router: Router,
-    private uploadSvc: UploadService,
-    private userSvc: UserService,
     private authSvc: AuthService
   ) { }
 
   ngOnInit() {
-    this.setAuthor();
-
-
     this.authSvc
       .authInfo$
       .subscribe(user => {
@@ -42,42 +31,21 @@ export class ArticlePreviewCardComponent implements OnInit {
       });
       if (this.articleData.imageUrl) {
         this.articleCoverImageUrl = this.articleData.imageUrl;
-      } else {
-        this.getArticleCoverImage(this.articleData.articleId);
       }
 
   }
 
-  // May be deprecated - are we even using author or the image any more?
-  async setAuthor() {
-    const author = await this.articleSvc.getAuthor(this.articleData.authorId);
-    if (author) {
-      author.uid = this.articleData.authorId;
-      this.author = author;
-      this.getProfileImage(author.uid);
-    }
-  }
 
   navigateToArticleDetail() {
     window.scrollTo(0, 0);
     this.articleSvc.navigateToArticleDetail(this.articleData.articleId);
   }
 
+
   navigateToProfile() {
     this.articleSvc.navigateToProfile(this.articleData.authorId);
   }
 
-  getProfileImage(uid) {
-    const basePath = 'uploads/profileImages/';
-    this.profileImageUrl = this.uploadSvc.
-     getImageUrl(uid, basePath);
-  }
-
-  async getArticleCoverImage(articleKey) {
-    const basePath = 'uploads/articleCoverImages/';
-    this.articleCoverImageUrl = await this.uploadSvc
-      .getImageUrl(articleKey, basePath);
-  }
 
   async checkIfBookmarked() {
     this.isArticleBookmarked = await this.articleSvc.isBookmarked(this.user.uid, this.articleData.articleId);
