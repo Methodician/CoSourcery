773de0b13269ae98e08e635525fe93f3abdb60e5
Methodician <jacob@flight.run>
2018-08-30 15:34:59 -0700

    The start of a better way to upload

--- src/app/components/shared/upload-form/upload-form.component.ts
+++ src/app/components/shared/upload-form/upload-form.component.ts
@@ -12,6 +12,7 @@ export class UploadFormComponent implements OnInit {
   currentUpload: Upload;
   selectedFiles: any;
   @Input() articleId;
+  // refactor: change uid to userId for clarity. (may also rename to image uplaod form and question wither we need a separate article-cover-image component for this)
   @Input() uid;
   @Input() articleStatus;
 
@@ -26,16 +27,31 @@ export class UploadFormComponent implements OnInit {
     return this.selectedFiles;
   }
 
-  setBasePath() {
+  uploadImage() {
+    const file = this.selectedFiles.item(0);
+    // using this.currentUpload so we can display progress... 
+    this.currentUpload = new Upload(file);
     if (!!this.articleId) {
-      const basePath = 'uploads/articleCoverImages/';
-      this.sendImgToUploadSvc(this.articleId, basePath);
-    } else {
-      const basePath = 'uploads/profileImages/';
-      this.sendImgToUploadSvc(this.uid, basePath);
+      this.upSvc.uploadArticleCoverImage(this.currentUpload, this.articleId, this.articleStatus);
+    }
+    else {
+      // may change this to not work for profile images, simplifying the logic.
+      this.sendImgToUploadSvc(this.uid, 'uploads/profileImages/');
     }
   }
 
+  // may become depricated
+  // setBasePath() {
+  //   if (!!this.articleId) {
+  //     const basePath = 'uploads/articleCoverImages/';
+  //     this.sendImgToUploadSvc(this.articleId, basePath);
+  //   } else {
+  //     const basePath = 'uploads/profileImages/';
+  //     this.sendImgToUploadSvc(this.uid, basePath);
+  //   }
+  // }
+
+  // may become deprecated
   sendImgToUploadSvc(key, basePath) {
     const file = this.selectedFiles.item(0);
     this.currentUpload = new Upload(file);
--- src/app/services/upload.service.ts
+++ src/app/services/upload.service.ts
@@ -11,42 +11,66 @@ export class UploadService {
   }
 
 
+  uploadArticleCoverImage(upload: Upload, articleId: string, newStatus?: boolean) {
+    const storageRef = this.storage.ref(`uploads/articleCoverImages/${articleId}`)
+    const task = storageRef.put(upload.file);
+
+    task.on(firebase.storage.TaskEvent.STATE_CHANGED, async () => {
+      const snap = task.snapshot;
+      upload.progress = ((snap.bytesTransferred / snap.totalBytes) * 100).toFixed(0);
+      const url = await storageRef.getDownloadURL();
+      const docRef = this.fsdb.doc(`articleData/articles/articles/${articleId}`);
+      const imageUrl = { imageUrl: url };
+      if (newStatus) {
+        docRef.set(imageUrl);
+        return url;
+      }
+      docRef.update(imageUrl);
+      return url;
+    }, error => {
+      console.error(error);
+      alert('trouble uploading the image ' + error.message);
+    }, () => {
+      console.log('article image upload success');
+    });
+  }
+
   // NEEDS REFACTOR
-  uploadImage(upload: Upload, uid, basePath, newStatus ?: boolean) {
+  uploadImage(upload: Upload, uid, basePath, newStatus?: boolean) {
     const storageRef = this.storage.ref(`${basePath}/${uid}`).put(upload.file);
     storageRef.on(firebase.storage.TaskEvent.STATE_CHANGED,
-     () => {
+      () => {
         const snap = storageRef.snapshot;
-          upload.progress = ((snap.bytesTransferred / snap.totalBytes) * 100).toFixed(0);
+        upload.progress = ((snap.bytesTransferred / snap.totalBytes) * 100).toFixed(0);
         snap.ref.getDownloadURL().then(url => {
           if (basePath === 'uploads/articleCoverImages/') {
-            if ( newStatus === true) {
-              this.fsdb.collection(`articleData/articles/articles/`).doc(`${uid}`).set({imageUrl: url});
+            if (newStatus === true) {
+              this.fsdb.collection(`articleData/articles/articles/`).doc(`${uid}`).set({ imageUrl: url });
               return url;
-            } else if ( newStatus === false) {
-              this.fsdb.collection(`articleData/articles/articles/`).doc(`${uid}`).update({imageUrl: url});
+            } else if (newStatus === false) {
+              this.fsdb.collection(`articleData/articles/articles/`).doc(`${uid}`).update({ imageUrl: url });
               return url;
             }
           } else {
             if (newStatus === true) {
-              this.rtdb.ref(`userInfo/open/${uid}`).set({imageUrl: url});
+              this.rtdb.ref(`userInfo/open/${uid}`).set({ imageUrl: url });
               return url;
             } else {
-              this.rtdb.ref(`userInfo/open/${uid}`).update({imageUrl: url});
+              this.rtdb.ref(`userInfo/open/${uid}`).update({ imageUrl: url });
               return url;
             }
           }
         });
-     },
-    (error) => {
-      console.log('error: ', error.message);
-      alert(error);
-    },
-    () => {
-      console.log('image upload success');
-    }
-  );
- }
+      },
+      (error) => {
+        console.log('error: ', error.message);
+        alert(error);
+      },
+      () => {
+        console.log('image upload success');
+      }
+    );
+  }
 
 
 
