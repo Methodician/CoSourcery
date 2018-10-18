22f52553c97aaff47868f17c4f0a8e4ba7405cbd
Jacob Johnston <jacob@flight.run>
2018-10-18 10:38:26 -0700

    s

--- src/app/admin/services/data-cleanup.service.ts
+++ src/app/admin/services/data-cleanup.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataCleanupService {
 
-  constructor() { }
+  constructor(
+    private afdb: AngularFireDatabase,
+    private afs: AngularFirestore
+  ) { }
+
+  getAllFsUsers() {
+    return this.afs.
+  }
 }
