44cfad0e4d0ac5cf488f41427dc9eaa920eee24e
Jacob Johnston <jacob@flight.run>
2018-11-01 13:49:49 -0700

    user-service tweaks

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,9 @@ import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import * as firebase from 'firebase';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage, AngularFireUploadTask, AngularFireStorageReference } from '@angular/fire/storage';
 
 
 @Injectable({
@@ -11,11 +14,14 @@ import * as firebase from 'firebase';
 export class UserService {
   private NULL_USER = new UserInfoOpen(null, null, null, null);
   userInfo$: BehaviorSubject<UserInfoOpen> = new BehaviorSubject<UserInfoOpen>(this.NULL_USER);
-  rtdb = firebase.database();
+  // rtdb = firebase.database();
   userMap: UserMap = {}
 
   constructor(
     private authSvc: AuthService,
+    private storage: AngularFireStorage,
+    private rtdb: AngularFireDatabase,
+    private fsdb: AngularFirestore
   ) {
     this.authSvc.authInfo$.subscribe(authInfo => {
       if (authInfo.uid) {
@@ -42,22 +48,52 @@ export class UserService {
 
   }
 
+  uploadTempImage(file: File): UploadTracker {
+    const id = this.fsdb.createId();
+    const storageRef = this.storage.ref(`tempImages/${id}`);
+    const task = storageRef.put(file);
+    return { task: task, ref: storageRef };
+  }
+
+  deleteFile(path: string) {
+    const storageRef = this.storage.ref(path);
+    storageRef.delete().subscribe(res => {
+      console.log('DELETED TEMP IMAGE (maybe), result: ', res);
+    });
+  }
+
+  uploadProfileImage(uid: string, file: File): UploadTracker {
+    const storageRef = this.storage.ref(`profileImages/${uid}`);
+    const task = storageRef.put(file);
+    return { task: task, ref: storageRef };
+  }
+
+  trackUploadedProfileImages(uid, fullPath, url) {
+    const docRef = this.fsdb.doc(`fileUploads/profileImages/${uid}`);
+    docRef.set({ path: fullPath, downloadUrl: url });
+  }
+
   setUserAccess(accessLevel: number, uid: string) {
-    return this.rtdb.ref
-      (`userInfo/accessLevel/${uid}`)
+    return this.rtdb
+      .object(`userInfo/accessLevel/${uid}`)
       .set(accessLevel);
   }
 
   createUser(userInfo, uid) {
     this.setUserAccess(10, uid);
     return this.rtdb
-      .ref(`userInfo/open/${uid}`)
+      .object(`userInfo/open/${uid}`)
       .set(userInfo);
   }
 
   async getUserInfo(uid) {
     if (uid) {
-      const userRef = await this.rtdb.ref(`userInfo/open/${uid}`).once(`value`);
+      try {
+
+      } catch (error) {
+
+      }
+      const userRef = await this.rtdb.object(`userInfo/open/${uid}`).query.once(`value`);
       const userData = userRef.val();
       return userData;
     }
@@ -65,3 +101,7 @@ export class UserService {
 
 }
 
+export interface UploadTracker {
+  task: AngularFireUploadTask,
+  ref: AngularFireStorageReference
+}
