64ee93dc275889f6fe10623b3b86886244a88266
Kristen <peanutster@gmail.com>
2018-11-08 13:04:42 -0800

    add countdown to timeout dialog box

--- src/app/components/shared/dialogs/edit-timeout/edit-timeout.component.ts
+++ src/app/components/shared/dialogs/edit-timeout/edit-timeout.component.ts
@@ -8,17 +8,28 @@ import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 })
 export class EditTimeoutComponent implements OnInit {
 
+  countDown = 45;
+  timeout;
+
   constructor(public dialogRef: MatDialogRef<EditTimeoutComponent>,
     @Inject(MAT_DIALOG_DATA) public data: {editing: boolean}) { }
 
   ngOnInit() {
+    this.timeout = setInterval(() => {
+      this.countDown--;
+      if(this.countDown === 0){
+        clearInterval(this.timeout);
+      }
+    }, 1000);
   }
 
   onNoClick(): void {
+    clearInterval(this.timeout);
     this.dialogRef.close(this.data);
   }
 
   onYesClick(): void {
+    clearInterval(this.timeout);
     this.data.editing = true;
     this.dialogRef.close(this.data);
   }
