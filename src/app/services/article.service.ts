acde866ef261d22e4f3d4856b66908bce86bc467
Jacob Johnston <jacob@flight.run>
2018-11-09 12:13:00 -0800

    Upgraded TypeScript and fixed up algolia imports

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -7,6 +7,7 @@ import { UserInfoOpen } from 'app/shared/class/user-info';
 import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
 import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { AngularFireStorage, AngularFireUploadTask, AngularFireStorageReference } from '@angular/fire/storage';
+import * as algoliasearch from 'algoliasearch/lite';
 import { environment } from 'environments/environment';
 
 @Injectable({
@@ -16,8 +17,7 @@ export class ArticleService {
   searchedArticles$ = new BehaviorSubject<ArticleDetailPreview[]>([]);
   fsServerTimestamp = firebase.firestore.FieldValue.serverTimestamp();
   dbServerTimestamp = firebase.database.ServerValue.TIMESTAMP;
-  algoliasearch = require('algoliasearch/dist/algoliasearch.js');
-  client = this.algoliasearch('7EELIYF04C', 'bb88a22504c5bc1a1f0ca58c7763a2b2');
+  algoliaClient = algoliasearch('7EELIYF04C', 'bb88a22504c5bc1a1f0ca58c7763a2b2');
 
   constructor(
     private storage: AngularFireStorage,
@@ -170,7 +170,7 @@ export class ArticleService {
   async searchArticles(query) {
     const articleList = new Array<any>();
     this.searchedArticles$.next(articleList);
-    const index = this.client.initIndex(environment.algoliaIndex); // using index dev articles for now, in production will want to change this.
+    const index = this.algoliaClient.initIndex(environment.algoliaIndex);
     const searchResults = await index.search({
       query: query,
       attributesToRetrieve: ['objectId'],
