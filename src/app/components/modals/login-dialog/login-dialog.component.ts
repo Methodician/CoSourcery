e536a5036379938c7bb626d35351d1f94ca5bcb9
Jacob Johnston <jacob@flight.run>
2018-11-12 14:13:08 -0800

    Handled login errors more flexibly

--- src/app/components/modals/login-dialog/login-dialog.component.ts
+++ src/app/components/modals/login-dialog/login-dialog.component.ts
@@ -29,8 +29,25 @@ export class LoginDialogComponent {
     this.authSvc.login(val.email, val.password).then(res => {
       this.dialogRef.close();
     }).catch(err => {
-      this.loginError = 'Your login email or password is incorrect.';
+      this.handleLoginError(err);
     });
+  }
+
+  handleLoginError(err) {
+    switch (err.code) {
+      case 'auth/user-not-found':
+        this.loginError = 'We couldn\'t find your account. Check your email for typos and try again or create an account.';
+        break;
+      case 'auth/invalid-email':
+        this.loginError = 'That doesn\'t look like a real email. Please try again or create an account.';
+        break;
+      case 'auth/wrong-password':
+        this.loginError = 'The password you entered doesn\'t match our records. If you forgot your password please contact info@flight.run.';
+        break;
+      default:
+        this.loginError = 'There was a problem logging in. We\'re not sure what happened. If you\'re sure hou have an account please contact info@flight.run';
+        break;
+    }
   }
 
   onCancel() {
