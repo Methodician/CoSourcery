e8d74bc4653ce819291dfdbfd78568ba1c61416c
Mike Chu <mcihkue@gmail.com>
2018-11-24 17:21:46 -0800

    Add component logic, template, and styles for ConfirmDialog.

--- src/app/components/modals/confirm-dialog/confirm-dialog.component.ts
+++ src/app/components/modals/confirm-dialog/confirm-dialog.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'cos-confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
   styleUrls: ['./confirm-dialog.component.scss']
 })
-export class ConfirmDialogComponent implements OnInit {
+export class ConfirmDialogComponent {
+  dialogTitle: string;
+  dialogLine1: string;
+  dialogLine2: string;
 
-  constructor() { }
+  constructor(
+    private dialogRef: MatDialogRef<ConfirmDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data: {
+      dialogTitle: string,
+      dialogLine1: string,
+      dialogLine2: string
+    }
+  ) {
+    this.dialogTitle = data.dialogTitle ? data.dialogTitle : 'Message Title';
+    this.dialogLine1 = data.dialogLine1 ? data.dialogLine1 : 'Message Line 1';
+    this.dialogLine2 = data.dialogLine2 ? data.dialogLine2 : null;
+  }
 
-  ngOnInit() {
+  onSelectNo(): void {
+    this.dialogRef.close(false);
+  }
+
+  onSelectYes(): void {
+    this.dialogRef.close(true);
   }
 
 }
