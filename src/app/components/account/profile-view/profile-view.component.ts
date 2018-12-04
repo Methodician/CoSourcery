56f5338980789d779b05067da7c27f5743f59e7d
Methodician <jacob@flight.run>
2018-12-03 16:42:19 -0800

    Since we're using the same styles, no need to use a separate stylesheet

--- src/app/components/account/profile-view/profile-view.component.ts
+++ src/app/components/account/profile-view/profile-view.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'cos-profile-view',
   templateUrl: './profile-view.component.html',
-  styleUrls: ['./profile-view.component.scss']
+  styleUrls: ['../profile-form/profile-form.component.scss']
 })
 export class ProfileViewComponent implements OnInit {
   @Input() user;
