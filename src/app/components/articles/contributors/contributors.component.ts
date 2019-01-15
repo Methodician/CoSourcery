58ae8634962d10704da4d17f3b45b32c4518ad34
Matt Krabacher <matt.krabacher@gmail.com>
2019-01-14 17:09:33 -0800

    created basic attribution scroll

--- src/app/components/articles/contributors/contributors.component.ts
+++ src/app/components/articles/contributors/contributors.component.ts
@@ -12,6 +12,12 @@ export class ContributorsComponent {
   _creatorId: string;
   _editors: Array<Object>;
   _editorMap: Object;
+  _displayEditors: Array<Object>;
+  _nextDisplayEditors: Array<Object>;
+  _prevDisplayEditors: Array<Object>;
+  displayEditorsPos: number;
+  transitionLeft = false;
+  transitionRight = false;
   @Input() set creatorId(creatorId: string) {
     if (creatorId && creatorId !== '') {
       this.mapCreator(creatorId);
@@ -22,19 +28,72 @@ export class ContributorsComponent {
     if (editorMap && editorMap !== {}) {
       const editors = this.mapContributors(editorMap);
       this._editors = editors;
+      this._prevDisplayEditors = [];
+      this._displayEditors = this._editors.slice(this.displayEditorsPos, 3);
+      this._nextDisplayEditors = this._editors.slice(this.displayEditorsPos + 3, 6);
       this._editorMap = editorMap;
     }
   }
   constructor(private userSvc: UserService) {
     this.userMap = userSvc.userMap;
+    this.displayEditorsPos = 0;
   }
 
   displayText(editorId) {
-    let text = `Edits: ${this._editorMap[editorId]}`
+    let text = `Edits: ${this._editorMap[editorId]}`;
     if (editorId === this._creatorId) {
       text = '(creator)' + text;
     }
     return text;
+  }
+
+  nextEditorPanel() {
+    this.transitionLeft = true;
+    // wait for profile cards to transition
+    setTimeout(() => {
+      this.transitionLeft = false;
+
+      // move displayed profile card position up three or reset if at the end
+      this.displayEditorsPos += 3;
+      if (this.displayEditorsPos >= this._editors.length) {
+        this.displayEditorsPos = 0;
+      }
+
+      // set and slice editors to get the prev, display and next segments of the _editors
+      this._prevDisplayEditors = this._displayEditors;
+      this._displayEditors = this._nextDisplayEditors;
+      this._nextDisplayEditors = this._editors.slice(this.displayEditorsPos + 3, this.displayEditorsPos + 6);
+
+      // if next is empty set it to the beginning of _editors
+      if (this._nextDisplayEditors.length <= 0) {
+        this._nextDisplayEditors = this._editors.slice(0, 3);
+      }
+    }, 2000);
+  }
+
+  prevEditorPanel() {
+    this.transitionRight = true;
+    // wait for profile cards to transition
+    setTimeout(() => {
+      this.transitionRight = false;
+
+      // move displayed profile card position up three or reset if at the end
+      this.displayEditorsPos -= 3;
+      if (this.displayEditorsPos < 0) {
+        this.displayEditorsPos = this._editors.length - 1;
+      }
+
+      // set and slice editors to get the prev, display and next segments of the _editors
+      this._nextDisplayEditors = this._displayEditors;
+      this._displayEditors = this._prevDisplayEditors;
+
+      // if prev is empty set it to the end of _editors
+      if (this.displayEditorsPos < 3) {
+        this._prevDisplayEditors = this._editors.slice(this._editors.length - (this._editors.length % 3), this._editors.length);
+      } else {
+        this._prevDisplayEditors = this._editors.slice(this.displayEditorsPos - 3, this.displayEditorsPos);
+      }
+    }, 2000);
   }
 
   mapCreator(creatorId: string) {
