11f489d59a113fcda431318bc74af4cd057c5d65
Matt Krabacher <matt.krabacher@gmail.com>
2019-01-20 16:20:05 -0800

    prevEditorPanel() works with dynamic panel count

--- src/app/components/articles/contributors/contributors.component.ts
+++ src/app/components/articles/contributors/contributors.component.ts
@@ -85,28 +85,36 @@ prevEditorPanel() {
   this.checkWindowSize();
   setTimeout(() => {
     this.transitionRight = false;
+    const remainder = this._editors.length % this.editorPanelCount;
     // move displayed profile card position up three or reset if at the end
     this.displayEditorsPos -= this.editorPanelCount;
-    if (this.displayEditorsPos < 0) {
-      this.displayEditorsPos = this._editors.length - 2;
+
+    // check that the DEP is still positive
+    if (this.displayEditorsPos < 0 && remainder > 0) {
+      // if not positive and if we have a remainder then we want the last remainder(amount) of profile cards from the _editors
+      this.displayEditorsPos = this._editors.length - remainder;
+    } else if (this.displayEditorsPos < 0) {
+      // if not positive and if we don't have a remainder then we want the last EPC(amount) of profile cards from the _editors
+      this.displayEditorsPos = this._editors.length - this.editorPanelCount;
     }
+
     // set and slice editors to get the prev, display and next segments of the _editors
     this._nextDisplayEditors = this._displayEditors;
     this._displayEditors = this._prevDisplayEditors;
-    console.log(this.displayEditorsPos);
-    // if prev is empty set it to the end of _editors
-    if (this.displayEditorsPos < 1) {
-      console.log('ran');
-      const remainder = this._editors.length % this.editorPanelCount;
-      if (remainder > 0) {
-        this._prevDisplayEditors = this._editors.slice(this._editors.length - (this._editors.length % this.editorPanelCount), this._editors.length);
-      } else {
-        this._prevDisplayEditors = this._editors.slice(this._editors.length - this.editorPanelCount, this._editors.length);
-      }
+
+    // check if prev position is below 0. i.e. if the DEP is less than the amount of cards on display then the prev panel
+    // position will be below 0 and thus should get profile cards from the end of the _editors array
+    if (this.displayEditorsPos < this.editorPanelCount && remainder > 0) {
+      // if the editorPanelCount does not divide evenly into the length of the _editors array then we take the remainder(number) from the end of the _editors array. 
+      // i.e. if we have 10 _editors and are displaying 3 cards per panel. the last panel should only have 1 card be cause 3 + 3 + 3 + 1(our remainder) = 10
+      this._prevDisplayEditors = this._editors.slice(this._editors.length - remainder, this._editors.length);
+    } else if (this.displayEditorsPos < this.editorPanelCount) {
+      // if the EPC does divide evenly then we want our last panel to have EPC amount of profile cards in it from the end of the _editors array
+      this._prevDisplayEditors = this._editors.slice(this._editors.length - this.editorPanelCount, this._editors.length);
     } else {
+      // and if the DEP is greater than the EPC then you just take the next EPC amount of profile cards from infront of the DEP index in _editors
       this._prevDisplayEditors = this._editors.slice(this.displayEditorsPos - this.editorPanelCount, this.displayEditorsPos);
     }
-
   }, 2000);
 }
 
