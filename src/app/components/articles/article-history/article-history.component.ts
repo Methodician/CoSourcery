532e0fd96fbaf12d620db5793200eb0c9d7bd0ef
Matt Krabacher <matt.krabacher@gmail.com>
2019-01-22 11:49:57 -0800

    expanded getArticleHistory() to also map contributor info to userMap

--- src/app/components/articles/article-history/article-history.component.ts
+++ src/app/components/articles/article-history/article-history.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleService } from '@services/article.service';
+import { UserService } from '@services/user.service';
 
 @Component({
   selector: 'cos-article-history',
@@ -13,11 +14,14 @@ export class ArticleHistoryComponent implements OnInit {
   articleVersion:string = null;
   articleHistory = {};
   articleHistoryKeys = [];
-
+  articleContributorIds: string[];
+  
+  displayedColumns: string[] = ['title', 'version', 'lastUpdated', 'lastEditorId'];
 
   constructor(
               private route: ActivatedRoute,
               private articleSvc: ArticleService,
+              private userSvc: UserService,
             ) { }
 
   ngOnInit() {
@@ -25,9 +29,7 @@ export class ArticleHistoryComponent implements OnInit {
     this.getArticleHistory();
   }
 
-
   // each times params change check key agains current displayed article key and if not the same, update.
-  
   setArticleId() {
     this.route.params.subscribe(params => {
       if (params['key'] && params['version']) {
@@ -35,18 +37,27 @@ export class ArticleHistoryComponent implements OnInit {
         this.articleId = key;
         this.articleVersion = version;
       }
-      // this.ckeditor.config.fbImageStorage = { storageRef: this.articleSvc.createVanillaStorageRef(`articleBodyImages/${this.articleId}/`) };
     });
   }
 
   getArticleHistory() {
     this.articleSvc.getArticleHistoryRefById(this.articleId).valueChanges().subscribe(history => {
-      // this.articleHistory = history;
+      // translate returned array into object map with version number for keys and version data for values
       history.map(article => {
         this.articleHistory[article.version] = article;
       });
+      
+      // create array of version numbers and sort them starting with the latest version
       this.articleHistoryKeys = Object.keys(this.articleHistory);
+      this.articleHistoryKeys.sort((key1, key2) => this.articleHistory[key1].version < this.articleHistory[key2].version ? 1 : -1)
 
+      // create array of contributor IDs and add them to the userMap in the UserService if they're not already there.
+      this.articleContributorIds = Object.keys(this.articleHistory[this.articleHistoryKeys[0]].editors)
+      this.articleContributorIds.map(id => {
+        if (!this.userSvc.userMap[id]) {
+          this.userSvc.addUserToMap(id); 
+        };
+      });
     });
   }
 }
