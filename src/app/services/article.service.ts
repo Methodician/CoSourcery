d0dbc335a9353593a3fad94fd657107bfaafc1a1
Methodician <jacob@flight.run>
2019-02-10 13:59:02 -0800

    Removed unused import

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
 import { BehaviorSubject } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import {
   ArticleDetail,
   ArticlePreview,
