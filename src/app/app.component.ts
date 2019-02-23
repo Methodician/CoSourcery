494c461b7ef90a71714b3e0dbd3afc8d8176ae07
Matt Krabacher <matt.krabacher@gmail.com>
2019-02-23 14:32:07 -0800

    added prepareRoute function to check that outlet is outlet to animate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import {
+  fader,
+} from './route-animations';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'cos-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    fader,
+  ]
 })
 export class AppComponent {
 
   constructor() { }
+
+  // this is optional if only using one animation for every route
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
 
 }
