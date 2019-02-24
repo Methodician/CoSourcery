2b1e6cfa2c7bc5b52f8d539a145d5b464a65a745
Matt Krabacher <matt.krabacher@gmail.com>
2019-02-23 16:22:08 -0800

    profile entry tranistion created

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -8,21 +8,31 @@ import {
 
 export const fader =
   trigger('routeAnimations', [
-    transition('* => Profile', [
-      query(':enter, :leave', [
+    // Enter profile transition
+    transition('* => Profile', [    // designates that on entry to Profile state from anywhere(*). State is set in routing module.
+      query(':leave', [
         style({
           position: 'absolute',
           left: 0,
           width: '100%',
+          opacity: 1,
+          'z-index': '-100',
+        }),
+      ]),
+      query(':enter', [
+        style({
+          position: 'absolute',
+          left: 0,
+          top: '-100vh',
+          width: '100%',
           opacity: 0,
-          transform: 'scale(0) translateY(100%)',
         }),
       ]),
       query(':enter', [
         animate('600ms ease',
           style( {
+            top: 0,
             opacity: 1,
-            transform: 'scale(1) translateY(0)',
           })
         )
       ]),
