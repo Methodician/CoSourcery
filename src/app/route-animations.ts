88e92976a2646b050e3d0bc78952277226c716ac
Matt Krabacher <matt.krabacher@gmail.com>
2019-02-23 16:35:37 -0800

    extended transition for debugging and re-enabled fade in

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -25,14 +25,43 @@ export const fader =
           left: 0,
           top: '-100vh',
           width: '100%',
-          // opacity: 0,
+          opacity: 0,
         }),
       ], { optional: true }),
       query(':enter', [
-        animate('600ms ease',
+        animate('2s ease',
           style( {
             top: 0,
             opacity: 1,
+          })
+        )
+      ], { optional: true }),
+    ]),
+    // Exit profile transition
+    transition('Profile => *', [
+      query(':enter', [
+        style({
+          position: 'absolute',
+          left: 0,
+          width: '100%',
+          opacity: 1,
+          'z-index': '-100',
+        }),
+      ], { optional: true }),
+      query(':leave', [
+        style({
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100%',
+          opacity: 1,
+        }),
+      ], { optional: true }),
+      query(':leave', [
+        animate('2s ease',
+          style( {
+            top: '-100vh',
+            opacity: 0,
           })
         )
       ], { optional: true }),
