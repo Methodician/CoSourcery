3455b13ce4ddf5141e1d42bad7ac5819229c05a9
Matt Krabacher <matt.krabacher@gmail.com>
2019-03-10 13:45:24 -0700

    now detecting window size to determine when to truncate username.

--- src/app/components/account/profile-card/profile-card.component.ts
+++ src/app/components/account/profile-card/profile-card.component.ts
@@ -11,10 +11,17 @@ export class ProfileCardComponent implements OnInit {
   @Input() user: UserInfoOpen;
   @Input() displayText: string;
   @Input() shouldHighlight = false;
+  windowMaxWidth = 780;
+  maxUsernameLength;
 
   constructor() { }
 
   ngOnInit() {
+    this.checkWindowSize();
+  }
+
+  checkWindowSize() {
+    window.innerWidth < this.windowMaxWidth ? this.maxUsernameLength = 6 : this.maxUsernameLength = 8;
   }
 
 }
