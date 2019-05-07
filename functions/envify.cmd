080bc89cd909169f7c67cf757b3904e3aaeb1be8
Methodician <methodician@gmail.com>
2019-05-06 17:56:02 -0700

    Removed strange Windows stuff fro functions

--- functions/envify.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\envify\bin\envify" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\envify\bin\envify" %*
-)
